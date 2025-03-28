f98f30b9a0745e669904521a6a4755f1f63ab8ab
Роман Семёнов <romansemenov2109@gmail.com>
2025-03-28 12:58:01 +0200

    add comment

--- modules/ai.py
+++ modules/ai.py
@@ -9,46 +9,57 @@ import io
 import random
 
 list_voices = ['alloy', 'ash', 'coral', 'echo', 'fable', 'onyx', 'nova', 'sage', 'shimmer']
-#
+# Загружаємо всі дані з .env файлу
 dotenv.load_dotenv()
 OPENAI_SECRET_KEY = os.getenv("OPENAI_SECRET_KEY")
 
-#
+# Створюємо асинхронний об'єкт для openAi
 client_openai = openai.AsyncOpenAI(api_key = OPENAI_SECRET_KEY)
 
-# 
+
 async def get_response_from_ai(question: str):
-    # 
+    """
+    функція яка отримує відповідь від штучного інтелекту за допомогою моделі gpt-4o-mini, яка навчена нашою інформауією
+    """
     response = await client_openai.chat.completions.create(
-        model = "ft:gpt-4o-mini-2024-07-18:worldit::BFf7bJwz", #
+        model = "ft:gpt-4o-mini-2024-07-18:worldit::BFf7bJwz", # Використовуємо нашу навчену модель
         messages = [{
-            "role": "user", #
-            "content": question, #
+            "role": "user", # Ставимо роль звичайного користувача
+            "content": question, # Ставимо щоб на вхід було питання
         }],
     )
-    # 
-    return response.choices[0].message.content
+    # Повертаємо відповідь на питання яке ми задали штучному інтелекту
+    return response.choices[0].message.content 
 
 
 async def get_image(prompt: str):
+    """
+    Створюємо асинхронну функцію котра генерує зображення за даними користувача
+    """ 
     response = await client_openai.images.generate(
-        model= "dall-e-2",
-        prompt= prompt,
-        size= "1024x1024",
-        quality="standard"
+        model= "dall-e-2", #  Вказуємо модель генерації
+        prompt= prompt, #  Те що буде генерувати
+        size= "1024x1024", # Розміри зображення
+        quality="standard" #  Якість зображення
     )
-
-    return response.data[0].url
+    # Отримуємо посилання на зображення
+    return response.data[0].url 
 
 
 async def get_voice(text: str):
+    """
+        Ця функція повертає аудіо файл с озвученим текстом
+    """
+    # Отримуємо рандомний голос зі списку list_voices
     speeker = random.choice(list_voices)
+    # відповідь від ШІ 
     response = await client_openai.audio.speech.create(
-        model = "gpt-4o-mini-tts",
-        voice= speeker,
-        input = text
+        model = "gpt-4o-mini-tts", # Версія моделі яка буде озвучувати текст
+        voice= speeker, # голос яким буде озвучуватись текст
+        input = text # той текст який потрібно озвучити
     )
     print(speeker, '- read your text')
-    
+    # перетворюємо звук з байт-коду у звук 
     audio_file = io.BytesIO(response.content)
+    # повертаємо цей звук
     return audio_file
--- modules/discord_bot.py
+++ modules/discord_bot.py
@@ -7,36 +7,62 @@ import discord
 import dotenv, os
 from .ai import get_response_from_ai, get_image, get_voice
 
+# Загружаємо всі дані з .env файлу
 dotenv.load_dotenv()
+
+# Записуємо в змінну TOKEN значення токена
 TOKEN = os.getenv("TOKEN")
 
+# Використовуємо базові налаштування
 intents = discord.Intents.default()
 intents.message_content = True
 
-bot_client = discord.Client(intents= intents)
+# Створюємо об'єкт класу з налаштуваннями
+bot_client = discord.Client(intents=intents)
 
 @bot_client.event
 async def on_ready():
+    """
+        Асинхронна функція, яка після запуску бота виводить у консоль, що бот запущений
+    """
     print(f"Бот {bot_client.user} запущено")
 
 @bot_client.event
 async def on_message(message: discord.Message):
+    """
+	Ця асинхронна функція обробляє вхідне повідомлення і в залежності від нього викликає певні функції, такі як створення фото, озвучення тексту або відповідь від ШІ
+    """
+    # Частина коду, яка виводить помилку і не зупиняє код
     try:
+        # Перевірка що повідомлення написав не цей бот
         if message.author != bot_client.user:
+            # З повідомлення бере лише потрібний нам текст
             content = message.content
-            message_for_answer = await message.channel.fetch_message(message.id)
+            # Дізнається id повідомленняна яке потрібно відповісти
+            message_for_answer = await message.channel.fetch_message(message.id) 
 
+            # Створюємо умову, що перевіряє, чи починається повідомлення із символів '!image'
             if content.startswith('!image'):
+                # Створюємо змінну для асинхронної відповіді у вигляді гіфки заванатаження, під час генерування картинки
                 load = await message_for_answer.reply('https://tenor.com/view/amalie-steiness-borregaard-loading-gif-loading-gif-25192894')
+                # Отримуємо увесь інший запит після символів '!image'
                 prompt = content[6:]
+                # Створюємо  асинхронну відвоідь - згенеровану картинку за запитом
                 response = await get_image(prompt = prompt)
-                await load.edit(content=response)
+                # В асинхронному режимі змінюємо гіфку завантаження на згенеровану картинку
+                await load.edit(content=response) 
             elif content.startswith("!voice"):
+                # З повідомлення виділяємо 
                 text = content[6:]
+                # Викликаємо функцію get_voice
                 response = await get_voice(text = text)
+                # Відповідає користувачу відповідь з аудіофайом
                 await message_for_answer.reply(content= f"Ось звук який ви отримали:", file= discord.File(response, filename= "speech.mp3"))
+            # Якщо не розпізнано як !voice та !image
             else:
+                # Отримаємо відповідь ШІ 
                 response = await get_response_from_ai(content)
-                await message_for_answer.reply(response)
+                # Відправляє відповідь на повідомлення
+                await message_for_answer.reply(response) 
     except:
         print("Error, message:", message)
