0276790610ce7f1c17c87bbde25b78c22a1661bd
Роман Семёнов <romansemenov2109@gmail.com>
2025-03-28 12:37:27 +0200

    created 2 more functions: generate image and generate voice by text

--- modules/ai.py
+++ modules/ai.py
@@ -5,7 +5,10 @@
 '''
 import openai
 import dotenv, os
+import io
+import random
 
+list_voices = ['alloy', 'ash', 'coral', 'echo', 'fable', 'onyx', 'nova', 'sage', 'shimmer']
 #
 dotenv.load_dotenv()
 OPENAI_SECRET_KEY = os.getenv("OPENAI_SECRET_KEY")
@@ -25,3 +28,27 @@ async def get_response_from_ai(question: str):
     )
     # 
     return response.choices[0].message.content
+
+
+async def get_image(prompt: str):
+    response = await client_openai.images.generate(
+        model= "dall-e-2",
+        prompt= prompt,
+        size= "1024x1024",
+        quality="standard"
+    )
+
+    return response.data[0].url
+
+
+async def get_voice(text: str):
+    speeker = random.choice(list_voices)
+    response = await client_openai.audio.speech.create(
+        model = "gpt-4o-mini-tts",
+        voice= speeker,
+        input = text
+    )
+    print(speeker, '- read your text')
+    
+    audio_file = io.BytesIO(response.content)
+    return audio_file
--- modules/discord_bot.py
+++ modules/discord_bot.py
@@ -5,7 +5,7 @@
 '''
 import discord
 import dotenv, os
-from .ai import get_response_from_ai
+from .ai import get_response_from_ai, get_image, get_voice
 
 dotenv.load_dotenv()
 TOKEN = os.getenv("TOKEN")
@@ -20,15 +20,23 @@ async def on_ready():
     print(f"Бот {bot_client.user} запущено")
 
 @bot_client.event
-async def on_message(message):
+async def on_message(message: discord.Message):
     try:
         if message.author != bot_client.user:
             content = message.content
-            # 
-            response = await get_response_from_ai(content)
-            # 
             message_for_answer = await message.channel.fetch_message(message.id)
-            # 
-            await message_for_answer.reply(response)
+
+            if content.startswith('!image'):
+                load = await message_for_answer.reply('https://tenor.com/view/amalie-steiness-borregaard-loading-gif-loading-gif-25192894')
+                prompt = content[6:]
+                response = await get_image(prompt = prompt)
+                await load.edit(content=response)
+            elif content.startswith("!voice"):
+                text = content[6:]
+                response = await get_voice(text = text)
+                await message_for_answer.reply(content= f"Ось звук який ви отримали:", file= discord.File(response, filename= "speech.mp3"))
+            else:
+                response = await get_response_from_ai(content)
+                await message_for_answer.reply(response)
     except:
         print("Error, message:", message)
